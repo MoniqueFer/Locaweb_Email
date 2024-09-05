8d571e3ed627476157665273952c420f146f9d74
MoniqueFer <138914572+MoniqueFer@users.noreply.github.com>
2024-09-05 09:55:12 -0300

    Initial Commit

---
+++ Locaweb_Email/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Locaweb_Email
+//
+//  Created by Monique Ferrarini on 05/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Locaweb_Email/Locaweb_EmailApp.swift
@@ -0,0 +1,17 @@
+//
+//  Locaweb_EmailApp.swift
+//  Locaweb_Email
+//
+//  Created by Monique Ferrarini on 05/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Locaweb_EmailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
